54073c16f5ad8f6f58a6d41ca76f13ccc44ec7c3
zahrae028 <fatimabk028@gmail.com>
2025-12-25 11:54:24 +0100

    Add Database class for MySQL connection handling

---
+++ config/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database
+{
+
+    private $host ;
+    private $username ;
+    private $password;
+    private $dbname ;
+
+    private $conn;
+
+    public function __construct($host ,$username , $password , $dbname)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+
+    }
+
+    public function connection(){
+
+       $this->conn  = new PDO("mysql:host={$this->host};dbname={$this->dbname}",$this->username,$this->password);
+       return $this->conn;
+    }
+}
+
+$test = new Database("localhost" , "root", "" ,"unity_care");
+$test2 = $test->connection();
+if ($test2) {
+    echo "wada7mad";
+}
+
+
+?>
